0881d3e8410798b1828deab961ca152d4607a3d1
Wallace Honorato da Silva <101594950+WallaceHS20@users.noreply.github.com>
2023-05-11 09:58:23 -0300

    Protocolador
    Criação do protocolador

---
+++ Protocolador.Fccr/src/test/java/com/fccr/sjc/demo/ProtocoladorFccrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fccr.sjc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocoladorFccrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
